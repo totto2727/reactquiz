8a1b6cfcf389058fecd89025550beaed2e8f7bc2
totto2727 <kaihatu.totto2727@gmail.com>
2020-09-05 15:44:58 +0900

    vscode Linux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Top from './screens/Top'
+import React from "react";
+import Top from "./screens/Top";
 import NavBar from "./components/NavBar";
 import Quiz from "./screens/Quiz";
-import {BrowserRouter,Switch,Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <div className="container">
-                <Switch>
-                    <Route exact path="/" component={Top} />
-                    <Route path="/quiz" component={Quiz} />
-                    <Route render={() => <h4>not found...</h4>} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+function App(props) {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
